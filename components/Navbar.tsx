e2a0b5c655920cec46d22eedc1c1e9982d25bcde
K-shir0 <kakishiro2190394@gmail.com>
2022-11-30 13:57:15 +0900

    update: navbar を改善

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,6 +14,28 @@ const ICTSCNavBar = () => {
             <li>
               <Link href={'/problems'}>問題</Link>
             </li>
+            <li>
+              <Link href={'/ranking'}>順位</Link>
+            </li>
+            <li>
+              <Link href={'/problems'}>参加者</Link>
+            </li>
+            <li tabIndex={0} className={'ml-4 dropdown dropdown-end'}>
+              <div>admin</div>
+              <ul tabIndex={0}
+                  className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 text-base-content">
+                <li>
+                  <a>チーム: ictsc</a>
+                </li>
+                <li>
+                  <a>マイページ</a>
+                </li>
+                <div className="divider my-0"/>
+                <li>
+                  <a>ログアウト</a>
+                </li>
+              </ul>
+            </li>
           </ul>
         </div>
       </div>
