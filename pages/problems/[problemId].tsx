448a9ad770cd9b3216d2e12753b5002b56755314
K-shir0 <kakishiro2190394@gmail.com>
2022-12-26 01:48:29 +0900

    update: frontmatter に対応

--- pages/problems/[problemId].tsx
+++ pages/problems/[problemId].tsx
@@ -3,13 +3,23 @@ import MarkdownPreview from "../../components/MarkdownPreview";
 import 'zenn-content-css';
 import ICTSCCard from "../../components/Card";
 
-const Problem = () => {
+// @ts-ignore
+import metadataParser from 'markdown-yaml-metadata-parser';
+
+const ProblemPage = () => {
   // TODO(k-shir0): problemId を取得するコード
   // const router = useRouter();
   // const {problemId} = router.query;
 
   // TODO(k-shir0): あとで消す
-  const content = `
+  const source = `---
+id: 1
+title: 問題タイトル
+point:
+  max: 100
+  solvedCriterion: 100
+---
+  
   # 問題タイトル
   
   ## サブタイトル
@@ -23,22 +33,26 @@ const Problem = () => {
   \`\`\`
   `;
 
+  const result = metadataParser(source);
+  const {title, point} = result.metadata;
+
   return (
       <>
         <ICTSCNavBar/>
         <div className={'container-ictsc'}>
           <div className={'flex flex-row items-end py-12'}>
-            <h1 className={'title-ictsc pr-4'}>問題タイトル</h1>
+            <h1 className={'title-ictsc pr-4'}>{title}</h1>
             満点
-            100 pt 採点基準
-            100 pt
+            {point.max} pt
+            採点基準
+            {point.solvedCriterion} pt
           </div>
           <ICTSCCard>
-            <MarkdownPreview content={content}/>
+            <MarkdownPreview content={result.content}/>
           </ICTSCCard>
         </div>
       </>
   )
 }
 
-export default Problem
+export default ProblemPage
--- pages/problems/index.tsx
+++ pages/problems/index.tsx
@@ -1,27 +1,33 @@
 import ICTSCNavBar from "../../components/Navbar";
 import ProblemCard from "../../components/ProblemCard";
+import {GetServerSideProps} from "next";
+import {Problem} from "../../types/Problem";
 
-const Problems = () => {
+interface Props {
+    problems: Problem[];
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const problemA = {id: "abc", content: "test"};
+  const problemB = {id: "def", content: "test"};
+  return {
+    props: {
+      problems: [problemA, problemB]
+    }
+  }
+}
+
+const Problems = ({problems}: Props) => {
   return (
       <>
         <ICTSCNavBar/>
         <h1 className={'title-ictsc text-center py-12'}>問題一覧</h1>
         <ul className={'grid grid-cols-4 gap-8 container-ictsc'}>
-          <li>
-            <ProblemCard/>
-          </li>
-          <li>
-            <ProblemCard/>
-          </li>
-          <li>
-            <ProblemCard/>
-          </li>
-          <li>
-            <ProblemCard/>
-          </li>
-          <li>
-            <ProblemCard/>
-          </li>
+          {problems.map((problem) => (
+              <li key={problem.id}>
+                <ProblemCard/>
+              </li>
+          ))}
         </ul>
       </>
   )
