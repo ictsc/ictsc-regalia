5238111685bc47cf47365a6902933dca4a8e9b15
K-shir0 <50326556+K-shir0@users.noreply.github.com>
2022-12-31 10:42:27 +0900

    fix: 問題が連番になるように修正 (#49)

--- components/ProblemCard.tsx
+++ components/ProblemCard.tsx
@@ -2,15 +2,16 @@ import {Problem} from "../types/Problem";
 import Link from "next/link";
 
 type Props = {
+  index: number
   problem: Problem
 }
 
-const ProblemCard = ({problem}: Props) => {
+const ProblemCard = ({index, problem}: Props) => {
   return (
       <Link href={`/problems/${problem.code}`}
             className={'border p-4 hover:bg-base-200 hover:cursor-pointer rounded-md shadow-sm min-h-[212px] justify-between flex flex-col'}>
         <div>
-          <span className={'font-bold text-2xl text-primary pr-2'}>1</span>
+          <span className={'font-bold text-2xl text-primary pr-2'}>{index}</span>
           <span className={'text-xl font-bold'}>{problem.title}</span>
         </div>
         <div>
--- pages/problems/index.tsx
+++ pages/problems/index.tsx
@@ -20,9 +20,9 @@ const Problems = () => {
         <ICTSCNavBar/>
         <h1 className={'title-ictsc text-center py-12'}>問題一覧</h1>
         <ul className={'grid grid-cols-4 gap-8 container-ictsc'}>
-          {problems && problems.map((problem) => (
+          {problems && problems.map((problem, index) => (
               <li key={problem.id}>
-                <ProblemCard problem={problem}/>
+                <ProblemCard index={index + 1} problem={problem}/>
               </li>
           ))}
         </ul>
