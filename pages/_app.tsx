02f0aa348ff09f693a96b9f2ef20c200dcf6f65c
K-shir0 <50326556+K-shir0@users.noreply.github.com>
2023-01-08 19:16:49 +0900

    add: Mermaid 記法に対応 (#85)
    * add: Mermaid 記法に対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import {useEffect} from "react";
+
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import localFont from '@next/font/local'
@@ -8,6 +10,43 @@ const notoSansJP = localFont({
 })
 
 export default function App({Component, pageProps}: AppProps) {
+  useEffect(() => {
+    // 埋め込み構文の対応に必要 (1)
+    import("zenn-embed-elements");
+
+    // 埋め込み構文の対応に必要 (2)
+    const getEmbeddedMessage = (n: any) => {
+      try {
+        const e = JSON.parse(n);
+        if (typeof e.type != "string") throw new Error("Bad Request");
+        if (typeof e.data != "object") throw new Error("Bad Request");
+        return e
+      } catch (e) {
+        return {type: "none", data: {}}
+      }
+    };
+    window.addEventListener("message", n => {
+      const e = getEmbeddedMessage(n.data);
+      switch (e.type) {
+        case"ready": {
+          const t = document.getElementById(e.data.id);
+          if (t instanceof HTMLIFrameElement) {
+            const a = {type: "rendering", data: {src: t.getAttribute("data-content")}};
+            t.contentWindow && t.contentWindow.postMessage && t.contentWindow.postMessage(JSON.stringify(a), "*")
+          }
+          break
+        }
+        case"resize": {
+          if (e.data.height <= 0) break;
+          const t = document.getElementById(e.data.id);
+          t instanceof HTMLIFrameElement && (t.height = `${e.data.height || 0}`);
+          break
+        }
+      }
+    });
+  }, []);
+
+
   return (
       <div data-theme='ictsc' className={`${notoSansJP.className}`}>
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,12 @@ const Home = () => {
   
   これは本文です。
   
-  \`\`\`
-  sudo hogehoge
+  \`\`\`mermaid
+  graph TB
+      A[Hard edge] -->|Link text| B(Round edge)
+      B --> C{Decision}
+      C -->|One| D[Result one]
+      C -->|Two| E[Result two]
   \`\`\`
   `;
 
