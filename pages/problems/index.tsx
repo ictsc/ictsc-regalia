599324e7944fc082e51bc047ab824cfa8c07f2df
K-shir0 <50326556+K-shir0@users.noreply.github.com>
2023-02-27 14:03:26 +0900

    update: 問題一覧ページで幅を小さくすると列の量を減らすようにした (#101)

--- pages/problems/index.tsx
+++ pages/problems/index.tsx
@@ -24,7 +24,11 @@ const Problems = () => {
       </Head>
       <ICTSCNavBar />
       <h1 className={"title-ictsc text-center py-12"}>問題一覧</h1>
-      <ul className={"grid grid-cols-4 gap-8 container-ictsc"}>
+      <ul
+        className={
+          "grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-8 container-ictsc"
+        }
+      >
         {problems &&
           problems.map((problem, index) => (
             <li key={problem.id}>
