4fa4c18a27efadc2589f832e21608b0389a2a411
K-shir0 <50326556+K-shir0@users.noreply.github.com>
2023-02-28 02:39:05 +0900

    update: Hideen ボタンを追加 (#114)

--- pages/team_info.tsx
+++ pages/team_info.tsx
@@ -11,7 +11,8 @@ import { site } from "../components/_const";
 const TeamInfo = () => {
   const { user, isLoading } = useAuth();
   const { ranking, loading: isRankingLoading } = useRanking();
-  const [isHidden, setIsHidden] = useState(true);
+  const [isSSHHidden, setIsSSHHidden] = useState(false);
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
 
   if (isLoading || isRankingLoading) {
     return (
@@ -78,17 +79,27 @@ const TeamInfo = () => {
           <label className={"label pt-12"}>
             <span className={"label-text"}>踏み台サーバへの接続情報</span>
           </label>
-          <div
-            className={
-              "flex justify-between input input-bordered items-center max-w-[336px]"
-            }
-            onClick={() => {
-              setIsHidden(!isHidden);
-            }}
-          >
-            <p className={`${!isHidden && "invisible"}`}>{ssh}</p>
+          <div className={"flex flex-row"}>
+            <div
+              className={
+                "flex justify-between input input-bordered items-center max-w-[360px] grow"
+              }
+            >
+              <p
+                className={`pr-2 ${isSSHHidden ? "select-none" : "select-all"}`}
+              >
+                {isSSHHidden ? "********" : ssh}
+              </p>
+              <button
+                onClick={() => {
+                  setIsSSHHidden(!isSSHHidden);
+                }}
+              >
+                {isSSHHidden ? <Eye /> : <EyeSlash />}
+              </button>
+            </div>
             <button
-              className={`link link-hover`}
+              className={`link link-hover pl-2`}
               onClick={(e) => {
                 e.stopPropagation();
                 navigator.clipboard.writeText(ssh);
@@ -100,14 +111,24 @@ const TeamInfo = () => {
           <label className={"label"}>
             <span className={"label-text"}>パスワード</span>
           </label>
-          <div
-            className={
-              "flex justify-between input input-bordered items-center max-w-[336px]"
-            }
-          >
-            ********
+          <div className={"flex flex-row"}>
+            {" "}
+            <div
+              className={`flex justify-between input input-bordered items-center max-w-[360px] grow ${
+                isPasswordHidden ? "select-none" : "select-all"
+              }`}
+            >
+              {isPasswordHidden ? "********" : bastionPassword}
+              <button
+                onClick={() => {
+                  setIsPasswordHidden(!isPasswordHidden);
+                }}
+              >
+                {isPasswordHidden ? <Eye /> : <EyeSlash />}
+              </button>
+            </div>
             <button
-              className={`link link-hover`}
+              className={`link link-hover pl-2`}
               onClick={() => {
                 navigator.clipboard.writeText(bastionPassword);
               }}
@@ -120,5 +141,44 @@ const TeamInfo = () => {
     </>
   );
 };
+
+const Eye = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+    />
+  </svg>
+);
+
+const EyeSlash = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+    />
+  </svg>
+);
 
 export default TeamInfo;
