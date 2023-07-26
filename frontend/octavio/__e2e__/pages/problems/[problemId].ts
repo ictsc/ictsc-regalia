db3c5bf097106b0166ef26251a69efffdc7eb69a
K-shir0 <kakishiro2190394@gmail.com>
2023-07-27 00:04:57 +0900

    fix: problemid -> problemId を修正

--- frontend/octavio/__e2e__/pages/problems/[problemId].ts
+++ frontend/octavio/__e2e__/pages/problems/[problemId].ts
@@ -1,14 +1,14 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 const ProblemPage = {
   goto: async (page: Page, code: string) => {
     await page.goto(`/problems/${code}`);
   },
   validate: async (page: Page, title: string) => {
-    await expect(page.locator(".title-ictsc")).toHaveText(title);
+    await expect(page.locator("h1")).toHaveText(title);
   },
   waitFormSelector: async (page: Page, title: string) => {
-    await page.waitForSelector(`.title-ictsc >> text=${title}`);
+    await page.waitForSelector(`h1 >> text=${title}`);
   },
 };
 
