a748504a4ef6f98e3a9c0e3f1d6ca60af037edd8
K-shir0 <kakishiro2190394@gmail.com>
2023-07-30 17:25:09 +0900

    add: storybook を追加

--- frontend/octavio/tailwind.config.js
+++ frontend/octavio/tailwind.config.js
@@ -3,6 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
+    "./stories/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
