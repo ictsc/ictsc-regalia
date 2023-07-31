048e6c3d4abf20eb94947c4bb990b2ad1d559bf0
K-shir0 <kakishiro2190394@gmail.com>
2023-07-31 19:59:57 +0900

    delete: CommonLayout を削除

--- frontend/octavio/app/page.tsx
+++ frontend/octavio/app/page.tsx
@@ -1,17 +1,18 @@
 import ICTSCCard from "@/components/Card";
 import MarkdownPreview from "@/components/MarkdownPreview";
+import ICTSCTitle from "@/components/Title";
 import { rule } from "@/components/_const";
-import CommonLayout from "@/layouts/CommonLayout";
 
 function Home() {
   return (
-    <CommonLayout title="ルール">
-      <div className="container-ictsc">
+    <>
+      <ICTSCTitle title="ルール" />
+      <main className="container-ictsc">
         <ICTSCCard>
           <MarkdownPreview content={rule} />
         </ICTSCCard>
-      </div>
-    </CommonLayout>
+      </main>
+    </>
   );
 }
 
