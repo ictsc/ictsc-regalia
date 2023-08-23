5af8558b23b0cbbdc43aad060bbcdf999d1d060e
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-08-23 16:55:38 +0900

    wrapping errors

--- backend/pkg/db/redis/operations.go
+++ backend/pkg/db/redis/operations.go
@@ -5,24 +5,25 @@ import (
 	"time"
 
 	"github.com/bytedance/sonic"
+	"github.com/ictsc/ictsc-outlands/backend/pkg/errors"
 )
 
 // Get 指定したキーの値を取得する
 func (r *Redis[K, V]) Get(ctx context.Context, key K) (*V, error) { // nolint:ireturn
 	keyStr, err := sonic.Marshal(key)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err)
 	}
 
 	valueStr, err := r.c.Get(ctx, string(keyStr)).Result()
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err)
 	}
 
 	var value V
 	err = sonic.Unmarshal([]byte(valueStr), &value)
 
-	return &value, err
+	return &value, errors.Wrap(err)
 }
 
 // Set 指定したキーに値を設定する
@@ -31,15 +32,15 @@ func (r *Redis[K, V]) Get(ctx context.Context, key K) (*V, error) { // nolint:ir
 func (r *Redis[K, V]) Set(ctx context.Context, key K, value V, ttl int) error {
 	keyStr, err := sonic.Marshal(key)
 	if err != nil {
-		return err
+		return errors.Wrap(err)
 	}
 
 	valueStr, err := sonic.Marshal(value)
 	if err != nil {
-		return err
+		return errors.Wrap(err)
 	}
 
 	err = r.c.Set(ctx, string(keyStr), string(valueStr), time.Millisecond*time.Duration(ttl)).Err()
 
-	return err
+	return errors.Wrap(err)
 }
