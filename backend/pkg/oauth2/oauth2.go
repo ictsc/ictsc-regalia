431bd7ff72c73f9655adebe6f4bdeb2cfc655cff
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-08-24 17:16:05 +0900

    added comments to oauth2 interface

--- backend/pkg/oauth2/oauth2.go
+++ backend/pkg/oauth2/oauth2.go
@@ -5,7 +5,12 @@ import "context"
 
 // Provider OAuth2認証プロバイダーインターフェース
 type Provider interface {
+	// AuthorizationURL フロントエンドをリダイレクトさせる認可URLを発行する
 	AuthorizationURL() (reqID, url string)
-	ExchangeCodeForToken(ctx context.Context, reqID, code string) (token string, err error)
+
+	// GetToken OAuthプロバイダーが発行したcodeをアクセストークンに変換する
+	GetToken(ctx context.Context, reqID, code string) (token string, err error)
+
+	// GetIdentity アクセストークンを使用して、プロバイダー上のIdentityを取得する
 	GetIdentity(ctx context.Context, token string) (identity string, err error)
 }
