c9c1c37dfd217e11ae685bf3768a350627478e40
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-08-25 13:57:17 +0900

    using pointer

--- backend/pkg/oauth2/oauth2.go
+++ backend/pkg/oauth2/oauth2.go
@@ -9,7 +9,7 @@ import (
 // Provider OAuth2認証プロバイダーインターフェース
 type Provider interface {
 	// AuthorizationURL フロントエンドをリダイレクトさせる認可URLを発行する
-	AuthorizationURL() (reqID string, url url.URL)
+	AuthorizationURL() (reqID string, url *url.URL)
 
 	// GetToken OAuthプロバイダーが発行したcodeをアクセストークンに変換する
 	GetToken(ctx context.Context, reqID, code string) (token string, err error)
