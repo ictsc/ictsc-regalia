7b2f630b7a338159825b122ccc0ca18cfd507c83
syuuya-nakatomi <m21107@g.metro-cit.ac.jp>
2023-08-29 23:54:03 +0900

    created package oauth2/discord

---
+++ backend/pkg/oauth2/discord/discord.go
@@ -0,0 +1,19 @@
+// Package oauth2 OAuth2認証プロバイダーインターフェース定義
+package discord
+
+import (
+	"context"
+	"net/url"
+)
+
+// Provider OAuth2認証プロバイダーインターフェース
+type Provider interface {
+	// AuthorizationURL フロントエンドをリダイレクトさせる認可URLを発行する
+	AuthorizationURL() (reqID string, url *url.URL)
+
+	// GetToken OAuthプロバイダーが発行したcodeをアクセストークンに変換する
+	GetToken(ctx context.Context, reqID, code string) (token string, err error)
+
+	// GetIdentity アクセストークンを使用して、プロバイダー上のIdentityを取得する
+	GetIdentity(ctx context.Context, token string) (identity string, err error)
+}
