d7e53e0c60c4a2fe10ee8b093cd9bd2a34efa7d3
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-04 10:18:32 +0900

    delete retry

--- backend/go.mod
+++ backend/go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/bytedance/sonic v1.10.2
 	github.com/cockroachdb/errors v1.11.1
 	github.com/redis/go-redis/v9 v9.4.0
-	github.com/stretchr/testify v1.8.2
 	golang.org/x/net v0.17.0
 	google.golang.org/protobuf v1.32.0
 )
@@ -19,7 +18,6 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
 	github.com/cockroachdb/redact v1.1.5 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/getsentry/sentry-go v0.26.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -27,11 +25,9 @@ require (
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.7.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/pkg/connect/client/client.go
+++ backend/pkg/connect/client/client.go
@@ -2,7 +2,6 @@
 package client
 
 import (
-	"context"
 	"crypto/tls"
 	"net"
 	"net/http"
@@ -11,21 +10,13 @@ import (
 	"golang.org/x/net/http2"
 )
 
-const defaultRetry = 10
-
-// Retry リトライ機能付きConnectクライアント
-type Retry[C any] struct {
-	client C
-	retry  int
-}
-
-// NewRetry Retryを生成
-func NewRetry[C any](
+// New connectのClientを生成
+func New[C any]( // nolint:ireturn
 	newFunc func(httpClient connect.HTTPClient, baseURL string, opts ...connect.ClientOption) C,
 	baseURL string,
-	retry int,
 	dev bool,
-) *Retry[C] {
+	opts ...connect.ClientOption,
+) C {
 	client := http.DefaultClient
 	// 開発環境の場合はh2c(TLS無しのHTTP/2)を有効にする
 	if dev {
@@ -39,32 +30,5 @@ func NewRetry[C any](
 		}
 	}
 
-	if retry == 0 {
-		retry = defaultRetry
-	}
-
-	return &Retry[C]{
-		client: newFunc(client, baseURL, connect.WithSendGzip()),
-		retry:  retry,
-	}
-}
-
-// Do リトライ付きでcallbackを実行
-func (c *Retry[C]) Do(ctx context.Context, callback func(ctx context.Context, client C) error) error {
-	var err error
-
-	for i := 0; i < c.retry; i++ {
-		err = callback(ctx, c.client)
-
-		switch {
-		case err == nil:
-			return nil
-		case connect.CodeOf(err) == connect.CodeUnavailable:
-			continue
-		default:
-			return err
-		}
-	}
-
-	return err
+	return newFunc(client, baseURL, append(opts, connect.WithSendGzip())...)
 }
