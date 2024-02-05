01f3d7f6c3f3ae29499c2eadeee04f1e225a73db
k-shir0 <kakishiro2190394@gmail.com>
2024-02-05 10:09:32 +0900

    [Duardo] add linter

--- frontend/duardo/app/layout.tsx
+++ frontend/duardo/app/layout.tsx
@@ -1,6 +1,9 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
-import "./globals.css"
+import React from "react";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
