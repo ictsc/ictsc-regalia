2f1cdf118b77123493dbfedfae8e2b99ef5e9ce6
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-14 18:35:59 +0900

    add tx interface

---
+++ backend/pkg/db/rdb/rdb.go
@@ -0,0 +1,12 @@
+// Package rdb RDBユーティリティー
+package rdb
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Tx RDBトランザクションインターフェース
+type Tx interface {
+	Do(ctx context.Context, options *sql.TxOptions, callBack func(context.Context) error) error
+}
