e668ffa550144d6722523a3b14fda95fb16a6cd0
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-22 04:00:42 +0900

    define error flags

---
+++ backend/internal/domain/error.go
@@ -0,0 +1,10 @@
+package domain
+
+import "github.com/ictsc/ictsc-outlands/backend/pkg/errors"
+
+var (
+	// ErrNotFound Not Foundエラー
+	ErrNotFound errors.Flag = "Not Found"
+	// ErrUnknown Unknownエラー
+	ErrUnknown errors.Flag = "Unknown"
+)
