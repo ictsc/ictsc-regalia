fedebb8b2b7a1acd553f0249cabc91a56c9294d5
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-27 02:54:18 +0900

    moved error flags to errors package

---
+++ backend/pkg/errors/flags.go
@@ -0,0 +1,11 @@
+package errors
+
+// nolint:gochecknoglobals
+var (
+	// ErrNotFound Not Foundエラー
+	ErrNotFound Flag = "Not Found"
+	// ErrUnknown Unknownエラー
+	ErrUnknown Flag = "Unknown"
+	// ErrBadArgument Bad Argumentエラー
+	ErrBadArgument Flag = "Bad Argument"
+)
