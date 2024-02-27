9bc7af74da0d32f9e6577e1489f7c0f5e7876fc2
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-28 08:22:24 +0900

    fix service interface

--- backend/internal/anita/service/user.go
+++ backend/internal/anita/service/user.go
@@ -5,15 +5,14 @@ import (
 
 	"github.com/ictsc/ictsc-outlands/backend/internal/anita/domain"
 	"github.com/ictsc/ictsc-outlands/backend/internal/anita/domain/value"
-	"github.com/oklog/ulid/v2"
 )
 
 // UserService ユーザーサービス
 type UserService interface {
 	ReadUser(ctx context.Context, id value.UserID) (*domain.User, error)
 	ReadUsers(ctx context.Context) ([]*domain.User, error)
-	ReadUsersByTeamID(ctx context.Context, teamID ulid.ULID) ([]*domain.User, error)
+	ReadUsersByTeamID(ctx context.Context, teamID value.TeamID) ([]*domain.User, error)
 	CreateUser(ctx context.Context, user *domain.User) error
-	UpdateUser(ctx context.Context, id value.UserID, name string) (*domain.User, error)
+	UpdateUser(ctx context.Context, id value.UserID, name value.UserName) (*domain.User, error)
 	DeleteUser(ctx context.Context, id value.UserID) error
 }
