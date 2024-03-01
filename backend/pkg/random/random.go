f058ac144a9478b31bd07c1467ed270205302224
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-03-01 21:07:52 +0900

    prepared random utility

---
+++ backend/pkg/random/random.go
@@ -0,0 +1,25 @@
+// Package random 乱数ユーティリティー
+package random
+
+import (
+	"crypto/rand"
+
+	"github.com/ictsc/ictsc-outlands/backend/pkg/errors"
+)
+
+// NewString 指定された長さのランダム文字列を生成
+func NewString(digit uint32) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$"
+
+	byteSlice := make([]byte, digit)
+	if _, err := rand.Read(byteSlice); err != nil {
+		return "", errors.Wrap(errors.ErrUnknown, err)
+	}
+
+	var result string
+	for _, v := range byteSlice {
+		result += string(letters[int(v)%len(letters)])
+	}
+
+	return result, nil
+}
