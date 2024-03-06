4fcb241adaca963536058c6cbe41c23d0fe0dcb1
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-03-06 18:05:15 +0900

    implemented team service

--- backend/internal/anita/service/team.go
+++ backend/internal/anita/service/team.go
@@ -8,21 +8,32 @@ import (
 	"github.com/ictsc/ictsc-outlands/backend/pkg/optional"
 )
 
+// CreateTeamArgs チーム作成引数
+type CreateTeamArgs struct {
+	Code      value.TeamCode
+	Name      value.TeamName
+	Org       value.TeamOrganization
+	Remaining value.TeamRemaining
+}
+
 // UpdateTeamArgs チーム更新引数
 type UpdateTeamArgs struct {
-	ID      value.TeamID
-	Code    optional.Of[value.TeamCode]
-	Name    optional.Of[value.TeamName]
-	Org     optional.Of[value.TeamOrganization]
-	Bastion optional.Of[value.Bastion]
+	ID        value.TeamID
+	Code      optional.Of[value.TeamCode]
+	Name      optional.Of[value.TeamName]
+	Org       optional.Of[value.TeamOrganization]
+	Remaining optional.Of[value.TeamRemaining]
+	Bastion   optional.Of[value.Bastion]
 }
 
 // TeamService チームサービス
 type TeamService interface {
 	ReadTeam(ctx context.Context, id value.TeamID) (*domain.Team, error)
 	ReadTeams(ctx context.Context) ([]*domain.Team, error)
-	CreateTeam(ctx context.Context, code value.TeamCode, name value.TeamName, org value.TeamOrganization) (*domain.Team, error)
+	ReadTeamByInvitationCode(ctx context.Context, code value.TeamInvitationCode) (*domain.Team, error)
+	CreateTeam(ctx context.Context, args CreateTeamArgs) (*domain.Team, error)
 	UpdateTeam(ctx context.Context, args UpdateTeamArgs) (*domain.Team, error)
 	DeleteTeam(ctx context.Context, id value.TeamID) error
+	ReadMembers(ctx context.Context, id value.TeamID) ([]*domain.User, error)
 	MoveMember(ctx context.Context, to value.TeamID, memberID value.UserID) error
 }
