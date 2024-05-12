4e4dfde399922314079dad542b21d0e2ed70fbc6
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2024-05-12 15:10:43 +0900

    Fix broken link in `init.sh`

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 if ! (type task >/dev/null 2>&1); then
   echo "下記公式ドキュメントを参考に、taskをインストールした後、もう一度init.shを実行してください"
-  echo "https://taskfile.dev/ja-JP/installation/"
+  echo "https://taskfile.dev/installation/"
 fi
 
 task init
