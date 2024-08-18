5766abf20179b09c43f239a75615d409dcbb2bf3
tosuke <13393900+tosuke@users.noreply.github.com>
2024-08-19 01:36:42 +0900

    feat: introduce tailwindcss

--- frontend/packages/competition/app/index.tsx
+++ frontend/packages/competition/app/index.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
---
+++ frontend/packages/competition/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
