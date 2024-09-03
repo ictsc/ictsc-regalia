1c9f4256b95de5f74ffaac1f24e278775ec3a00c
tosuke <13393900+tosuke@users.noreply.github.com>
2024-09-03 17:09:01 +0900

    fix: example text

--- frontend/packages/contestant/app/components/app-shell/view.stories.tsx
+++ frontend/packages/contestant/app/components/app-shell/view.stories.tsx
@@ -21,13 +21,7 @@ function AppShell() {
     >
       <h1>Main</h1>
       <p>
-        {`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.`.repeat(100)}
+        あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
       </p>
     </Layout>
   );
