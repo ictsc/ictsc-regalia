f7a226e0500d24db412b30c79fe097547c212019
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-04 18:01:55 +0900

    chore: clean

--- backend/pkg/db/cache/cache.go
+++
@@ -1,9 +0,0 @@
-package cache
-
-import "context"
-
-// DB キャッシュ用データベースインターフェス
-type DB[V any] interface {
-	Get(ctx context.Context, key string) (*V, error)
-	Set(ctx context.Context, key string, value V, ttl int) error
-}
