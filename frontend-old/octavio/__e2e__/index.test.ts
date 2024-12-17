b6951c10b75ee9d4b57a1b844bf196a111a88b68
tosuke <13393900+tosuke@users.noreply.github.com>
2024-12-17 19:59:58 +0900

    chore: remove old files

--- frontend-old/octavio/__e2e__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "@playwright/test";
-
-import IndexPage from "./pages/Index";
-
-test("画面項目が表示されること", async ({ page }) => {
-  await IndexPage.goto(page);
-  await IndexPage.validate(page);
-});
--- frontend-old/octavio/components/title.tsx
+++
@@ -1,5 +0,0 @@
-function ICTSCTitle({ title }: { title: string }) {
-  return <h1 className="title-ictsc text-center py-12">{title}</h1>;
-}
-
-export default ICTSCTitle;
