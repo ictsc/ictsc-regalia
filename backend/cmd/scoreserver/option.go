aa27aee1d117ee6ffe63ceed351e3a03074f2ff2
tosuke <13393900+tosuke@users.noreply.github.com>
2025-01-13 20:59:32 +0900

    feat: policy enforcement

--- backend/cmd/scoreserver/option.go
+++ backend/cmd/scoreserver/option.go
@@ -15,8 +15,10 @@ type CLIOption struct {
 	LogLevel       slog.Level
 
 	AdminHTTPAddr       netip.AddrPort
-	AdminAuthConfigFile string
 	AdminAuthConfig     string
+	AdminAuthConfigFile string
+	AdminAuthPolicy     string
+	AdminAuthPolicyFile string
 }
 
 // NewOption creates a new CLIOption combined with the given flag.FlagSet.
@@ -32,6 +34,8 @@ func NewOption(fs *flag.FlagSet) *CLIOption {
 	fs.TextVar(&opt.AdminHTTPAddr, "admin.http-addr", netip.MustParseAddrPort("127.0.0.1:8081"), "Admin HTTP server address")
 	fs.StringVar(&opt.AdminAuthConfig, "admin.auth-config", "", "Admin API authentication config")
 	fs.StringVar(&opt.AdminAuthConfigFile, "admin.auth-config-file", "", "Admin API authentication config file")
+	fs.StringVar(&opt.AdminAuthPolicy, "admin.auth-policy", "", "Admin API authorization policy")
+	fs.StringVar(&opt.AdminAuthPolicyFile, "admin.auth-policy-file", "", "Admin API authorization policy file")
 
 	fs.BoolFunc("v", "Verbose logging", func(string) error {
 		opt.LogLevel = slog.LevelDebug
--- backend/scoreserver/admin/server_test.go
+++ backend/scoreserver/admin/server_test.go
@@ -1,11 +1,15 @@
 package admin_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"connectrpc.com/connect"
 	"github.com/ictsc/ictsc-regalia/backend/pkg/pgtest"
+	"github.com/ictsc/ictsc-regalia/backend/scoreserver/admin/auth"
+	"github.com/ictsc/ictsc-regalia/backend/scoreserver/config"
 )
 
 func TestMain(m *testing.M) {
@@ -24,4 +28,37 @@ func assertCode(t *testing.T, expected connect.Code, got error) {
 	if code != expected {
 		t.Errorf("expect code: %v, but got: %v", expected, got)
 	}
+}
+
+func setupEnforcer(tb testing.TB) *auth.Enforcer {
+	tb.Helper()
+
+	enforcer, err := auth.NewEnforcer(config.AdminAuthz{
+		Policy: "g, tester, role:admin",
+	})
+	if err != nil {
+		tb.Fatalf("Failed to create enforcer: %v", err)
+	}
+	return enforcer
+}
+
+func setupServer(tb testing.TB, handler http.Handler) *httptest.Server {
+	handler = auth.WithAuthn(handler, &staticAuthenticator{
+		Viewer: auth.Viewer{
+			Name: "tester",
+			Groups: []string{"tester"},
+		},
+	})
+	server := httptest.NewServer(handler)
+	tb.Cleanup(server.Close)
+	return server
+}
+
+type staticAuthenticator struct {
+	Viewer auth.Viewer
+}
+
+func (a *staticAuthenticator) HandleRequest(*http.Request) (*auth.Viewer, error) {
+	v := a.Viewer
+	return &v, nil
 }
