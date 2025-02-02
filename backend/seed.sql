959838e263c27df61a98d8397f0e0ef6022236aa
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-03 02:02:34 +0900

    fix: trim timestamps

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,8 +1,8 @@
 TRUNCATE TABLE teams CASCADE;
 INSERT INTO teams (id, code, name, organization, created_at, updated_at) VALUES
-    ('a1de8fe6-26c8-42d7-b494-dea48e409091', 1, 'トラブルシューターズ', 'ICTSC Association', '2025-02-02 08:00:13.570974+00', '2025-02-02 08:00:13.570974+00'),
-    ('83027d5e-fa32-41d6-b290-fc38ba337f89', 2, 'トラブルメイカーズ', 'ICTSC Association', '2025-02-02 08:00:13.570974+00', '2025-02-02 08:00:13.570974+00');
+    ('a1de8fe6-26c8-42d7-b494-dea48e409091', 1, 'トラブルシューターズ', 'ICTSC Association', '2025-02-02 08:00:00+00', '2025-02-02 08:00:00+00'),
+    ('83027d5e-fa32-41d6-b290-fc38ba337f89', 2, 'トラブルメイカーズ', 'ICTSC Association', '2025-02-02 08:00:00+00', '2025-02-02 08:00:00+00');
 
 TRUNCATE TABLE invitation_codes CASCADE;
-INSERT INTO invitation_codes (id, team_id, code, expires_at, created_at) VALUES
-    ('ad3f83d3-65be-4884-8a03-adb11a8127ef', 'a1de8fe6-26c8-42d7-b494-dea48e409091', 'LHNZXGSF7L59WCG9', '2025-02-02 08:10:00.389321+00', '2038-04-02 15:00:00+00');
+INSERT INTO invitation_codes (id, team_id, code, created_at, expires_at) VALUES
+    ('ad3f83d3-65be-4884-8a03-adb11a8127ef', 'a1de8fe6-26c8-42d7-b494-dea48e409091', 'LHNZXGSF7L59WCG9', '2025-02-02 08:10:00+00', '2038-04-02 15:00:00+00');
