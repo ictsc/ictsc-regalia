9efcd564f7398c2ccf542b60225abbb3e0a4f25b
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-03 21:00:46 +0900

    feat(infra/pg): add iterator util

--- backend/scoreserver/infra/pg/repository_test.go
+++ backend/scoreserver/infra/pg/repository_test.go
@@ -1,8 +1,21 @@
 package pg_test
 
+import "iter"
+
 func must[T any](v T, err error) T {
 	if err != nil {
 		panic(err)
 	}
 	return v
+}
+
+func collectErrIter[V any](iter iter.Seq2[V, error]) ([]V, error) {
+	slice := make([]V, 0)
+	for v, err := range iter {
+		if err != nil {
+			return nil, err
+		}
+		slice = append(slice, v)
+	}
+	return slice, nil
 }
