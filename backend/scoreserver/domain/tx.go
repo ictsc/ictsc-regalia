696f94d98508e5704faf6cdbfc9ed191b2570d3d
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-13 02:00:58 +0900

    feat(domain): add helper to handling tx

--- backend/scoreserver/domain/tx.go
+++ backend/scoreserver/domain/tx.go
@@ -1,6 +1,10 @@
 package domain
 
-import "context"
+import (
+	"context"
+
+	"github.com/cockroachdb/errors"
+)
 
 type Tx[E any] interface {
 	RunInTx(ctx context.Context, f func(E) error) error
@@ -10,4 +14,17 @@ type TxFunc[E any] func(context.Context, func(E) error) error
 
 func (t TxFunc[E]) RunInTx(ctx context.Context, f func(E) error) error {
 	return t(ctx, f)
+}
+
+func RunTx[V any, E any](ctx context.Context, tx Tx[E], fun func(E) (V, error)) (V, error) {
+	var val V
+	if tx == nil {
+		return val, errors.New("tx is nil")
+	}
+	err := tx.RunInTx(ctx, func(e E) error {
+		var err error
+		val, err = fun(e)
+		return err
+	})
+	return val, err
 }
