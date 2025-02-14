0bb55a0e4f30eba37640e507dad8ffa2135e2247
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-14 22:28:47 +0900

    fix: layouts w/o navbar

--- frontend/packages/contestant/app/components/app-shell/index.tsx
+++ frontend/packages/contestant/app/components/app-shell/index.tsx
@@ -1,13 +1,5 @@
-import {
-  type ReactNode,
-  startTransition,
-  use,
-  useDeferredValue,
-  useEffect,
-  useReducer,
-} from "react";
+import { type ReactNode, use, useDeferredValue, useReducer } from "react";
 import { type User } from "@app/features/viewer";
-import { useNavigate, useRouterState } from "@tanstack/react-router";
 import { Layout } from "./layout";
 import { Header } from "./header";
 import { NavbarView } from "./navbar";
@@ -24,54 +16,16 @@ export function AppShell({ children, viewer }: AppShellProps) {
   const [collapsed, toggle] = useReducer((o) => !o, false);
 
   return (
-    <>
-      <Layout
-        header={<Header user={viewer} />}
-        navbar={
-          defferedViewer?.type === "contestant" && (
-            <NavbarView collapsed={collapsed} onOpenToggleClick={toggle} />
-          )
-        }
-        navbarCollapsed={collapsed}
-      >
-        {children}
-      </Layout>
-      <Redirector viewer={defferedViewer} />
-    </>
+    <Layout
+      header={<Header user={viewer} />}
+      navbar={
+        defferedViewer?.type === "contestant" ? (
+          <NavbarView collapsed={collapsed} onOpenToggleClick={toggle} />
+        ) : null
+      }
+      navbarCollapsed={collapsed}
+    >
+      {children}
+    </Layout>
   );
-}
-
-function Redirector({ viewer }: { viewer: User | undefined }) {
-  const routerState = useRouterState();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (viewer == null) return;
-    switch (viewer.type) {
-      case "unauthenticated": {
-        if (routerState.location.pathname !== "/signin") {
-          startTransition(() =>
-            navigate({
-              to: "/signin",
-              search: { next: routerState.location.pathname },
-            }),
-          );
-        }
-        break;
-      }
-      case "pre-signup": {
-        if (routerState.location.pathname !== "/signup") {
-          startTransition(() =>
-            navigate({
-              to: "/signup",
-              search: { next: routerState.location.pathname },
-            }),
-          );
-        }
-        break;
-      }
-    }
-  }, [routerState, navigate, viewer]);
-
-  return null;
 }
