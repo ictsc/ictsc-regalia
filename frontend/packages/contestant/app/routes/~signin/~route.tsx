a657e29bc704cafce06ac4227710f7b2f59b9d74
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-14 22:45:04 +0900

    feat: use signin page in /signin

--- frontend/packages/contestant/app/routes/~signin/~route.tsx
+++ frontend/packages/contestant/app/routes/~signin/~route.tsx
@@ -1,4 +1,8 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { startTransition, Suspense, use, useDeferredValue, useEffect } from "react";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { type User } from "@app/features/viewer";
+import { Route as RootRoute } from "../~__root";
+import { SignInPage } from "./page";
 
 type SignInSearch = {
   next?: string;
@@ -14,12 +18,33 @@ export const Route = createFileRoute("/signin")({
 });
 
 function RouteComponent() {
+  const { viewer } = RootRoute.useLoaderData();
+
+  return (
+    <Suspense>
+      <Page viewerPromise={viewer} />
+    </Suspense>
+  );
+}
+
+function Page({ viewerPromise }: { viewerPromise: Promise<User> }) {
   const { next: nextPath = "/" } = Route.useSearch();
+
+  // 既にログインしているならそのまま next に遷移する
+  // ログインしているかどうかはログインの操作が可能かどうかには関係ない
+  // そのためログインしているかの確認の前にページ自体は表示しておき，状態が確定したら遷移するか判断する
+  const navigate = useNavigate();
+  const deferredViewer = useDeferredValue(viewerPromise, null);
+  const viewer = deferredViewer != null ? use(deferredViewer) : null;
+  useEffect(() => {
+    if (viewer == null) return;
+    if (viewer.type !== "unauthenticated") {
+      startTransition(() => navigate({ to: nextPath }));
+    }
+  }, [viewer, navigate, nextPath]);
+
   const authURL = new URL("/api/auth/signin", window.location.origin);
   authURL.searchParams.set("next", nextPath);
-  return (
-    <div>
-      <a href={authURL.toString()}>Sign in</a>
-    </div>
-  );
+
+  return <SignInPage signInURL={authURL.toString()} />;
 }
