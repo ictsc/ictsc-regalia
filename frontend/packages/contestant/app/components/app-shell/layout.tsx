490de0286d6b01b9df5b32001a39f6cf974afef6
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-22 03:39:20 +0900

    fix(problem): layout /problems

--- frontend/packages/contestant/app/components/app-shell/layout.tsx
+++ frontend/packages/contestant/app/components/app-shell/layout.tsx
@@ -30,7 +30,7 @@ export function Layout({
       <nav className="sticky top-[70px] col-start-1 col-end-2 row-start-2 row-end-3 h-[calc(100vh-70px)]">
         {navbar}
       </nav>
-      <main className="col-start-2 col-end-3 row-start-2 row-end-3 overflow-y-auto">
+      <main className="col-start-2 col-end-3 row-start-2 row-end-3 overflow-x-clip overflow-y-auto">
         {children}
       </main>
     </div>
--- frontend/packages/contestant/app/routes/~problems.index/page.tsx
+++ frontend/packages/contestant/app/routes/~problems.index/page.tsx
@@ -7,21 +7,23 @@ type PageProps = {
 
 export function ProblemsPage(props: PageProps) {
   return (
-    <ul className="grid grid-flow-row grid-cols-1 gap-x-40 gap-y-24 lg:grid-cols-2">
-      {props.problems.map((problem) => (
-        <li key={problem.code}>
-          <ProblemItem
-            code={problem.code}
-            title={problem.title}
-            maxScore={problem.maxScore}
-            score={problem.score?.score}
-            rawScore={problem.score?.markedScore}
-            penalty={problem.score?.penalty}
-            fullScore={problem.score?.score === problem.maxScore}
-            rawFullScore={problem.score?.markedScore === problem.maxScore}
-          />
-        </li>
-      ))}
-    </ul>
+    <div className="mx-16 mt-64 flex justify-center">
+      <ul className="grid grid-flow-row grid-cols-1 gap-x-40 gap-y-24 lg:grid-cols-2">
+        {props.problems.map((problem) => (
+          <li key={problem.code}>
+            <ProblemItem
+              code={problem.code}
+              title={problem.title}
+              maxScore={problem.maxScore}
+              score={problem.score?.score}
+              rawScore={problem.score?.markedScore}
+              penalty={problem.score?.penalty}
+              fullScore={problem.score?.score === problem.maxScore}
+              rawFullScore={problem.score?.markedScore === problem.maxScore}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
