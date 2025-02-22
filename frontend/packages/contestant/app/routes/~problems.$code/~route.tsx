8fe3f88ae6ef1b28a967728cef899a9f0ebc1943
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-23 00:38:29 +0900

    feat(problem): implement problem fetching logic

--- frontend/packages/contestant/app/routes/~problems.$code/~route.tsx
+++ frontend/packages/contestant/app/routes/~problems.$code/~route.tsx
@@ -1,9 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { fetchProblem } from "@app/features/problem";
+import { lazy, useDeferredValue } from "react";
 
 export const Route = createFileRoute("/problems/$code")({
   component: RouteComponent,
+  loader: ({ context: { transport }, params: { code } }) => ({
+    problem: fetchProblem(transport, code),
+  }),
+});
+
+const ProblemPage = lazy(async () => {
+  const mod = await import("./page");
+  return { default: mod.ProblemPage };
 });
 
 function RouteComponent() {
-  return <div>Hello /problems/$code!</div>;
+  const { problem } = Route.useLoaderData();
+  const deferredProblem = useDeferredValue(problem);
+  return <ProblemPage problem={deferredProblem} />;
 }
