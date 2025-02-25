5235fd71a9534cf9edbb66ac9d0da6d912ec2f48
tosuke <13393900+tosuke@users.noreply.github.com>
2025-02-25 15:12:34 +0900

    feat: lazy load katex styles

--- frontend/packages/contestant/app/components/markdown/markdown.tsx
+++ frontend/packages/contestant/app/components/markdown/markdown.tsx
@@ -1,9 +1,9 @@
 import { clsx } from "clsx";
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { type Components } from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import "katex/dist/katex.min.css";
+import katexCSS from "katex/dist/katex.min.css?url";
 import styles from "./markdown.module.css";
 
 export function Typography(props: {
@@ -19,10 +19,30 @@ export function Typography(props: {
 
 const remarkPlugins = [remarkGfm, remarkMath];
 const rehypePlugins = [rehypeKatex];
+const markdownComponents: Components = {
+  span: (props) => {
+    const { node: _node, ...elProps } = props;
+    const el = <span {...elProps} />;
+
+    if (elProps.className?.includes("katex")) {
+      return (
+        <>
+          <link rel="stylesheet" precedence="low" href={katexCSS} />
+          {el}
+        </>
+      );
+    }
+    return el;
+  },
+};
 
 export function Markdown({ children }: { children?: string }) {
   return (
-    <ReactMarkdown remarkPlugins={remarkPlugins} rehypePlugins={rehypePlugins}>
+    <ReactMarkdown
+      remarkPlugins={remarkPlugins}
+      rehypePlugins={rehypePlugins}
+      components={markdownComponents}
+    >
       {children}
     </ReactMarkdown>
   );
