e6516dda2b6755607613e98eeac00e9e4da32675
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-04 18:36:16 +0900

    refactor: appshell

--- frontend/packages/contestant/app/components/app-shell/index.ts
+++ frontend/packages/contestant/app/components/app-shell/index.ts
@@ -1,4 +1,5 @@
-export { NavbarLayoutContext, Layout } from "./layout";
+export { NavbarLayoutContext } from "./context";
+export { Layout } from "./layout";
 export { Header } from "./header";
 export { Navbar } from "./navbar";
 export { AccountMenu } from "./account-menu";
