f606807f9edc603d3bcfcbdf130508b56c5cf227
kouxi08 <kouxi14@gmail.com>
2025-03-04 16:20:51 +0900

    fix lint

--- frontend/packages/contestant/app/routes/~ranking/page.stories.tsx
+++ frontend/packages/contestant/app/routes/~ranking/page.stories.tsx
@@ -9,63 +9,63 @@ type Story = StoryObj;
 
 export const Default: Story = {
   render: () => (
-    <div className="gap-64 w-full">
+    <div className="w-full gap-64">
       <RankingItem
         rank={1}
         teamName="チーム名なまえがわからない"
         affiliation="testTeam"
         score={8888}
-        timeStamp={new Date('2025-03-04T12:00:00')}
+        timeStamp={new Date("2025-03-04T12:00:00")}
         penalty={0}
         fullScore
         rawFullScore
       />
-       <RankingItem
+      <RankingItem
         rank={2}
         teamName="チーム名がわからないいいいいいいいいいいいい"
         affiliation="testTeam"
         score={8888}
-        timeStamp={new Date('2025-03-04T12:00:00')}
+        timeStamp={new Date("2025-03-04T12:00:00")}
         penalty={0}
         fullScore
         rawFullScore
       />
-       <RankingItem
+      <RankingItem
         rank={3}
         teamName="チーム名がわからないいいいいいいいいいいいい"
         affiliation="testTeam"
         score={8888}
-        timeStamp={new Date('2025-03-04T12:00:00')}
+        timeStamp={new Date("2025-03-04T12:00:00")}
         penalty={0}
         fullScore
         rawFullScore
       />
-       <RankingItem
+      <RankingItem
         rank={4}
         teamName="チーム名がわからないいいいいいいいいいいいい"
-       affiliation="testTeam"
+        affiliation="testTeam"
         score={8888}
-        timeStamp={new Date('2025-03-04T12:00:00')}
+        timeStamp={new Date("2025-03-04T12:00:00")}
         penalty={0}
         fullScore
         rawFullScore
       />
-       <RankingItem
+      <RankingItem
         rank={5}
         teamName="チーム名がわからないいいいいいいいいいいいい"
-         affiliation="testTeam"
+        affiliation="testTeam"
         score={500}
-        timeStamp={new Date('2025-03-04T12:00:00')}
+        timeStamp={new Date("2025-03-04T12:00:00")}
         penalty={0}
         fullScore
         rawFullScore
       />
-       <RankingItem
+      <RankingItem
         rank={6}
         teamName="チーム名がわからないいいいいいいいいいいいい"
         affiliation="testTeam"
         score={0}
-        timeStamp={new Date('2025-03-04T12:00:00')}
+        timeStamp={new Date("2025-03-04T12:00:00")}
         penalty={0}
         fullScore
         rawFullScore
