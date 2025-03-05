77c5b461739c79b17741a0bee78d79d8850e92e2
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-05 16:51:21 +0900

    fix: format

--- frontend/packages/contestant/app/routes/~team-list/page.tsx
+++ frontend/packages/contestant/app/routes/~team-list/page.tsx
@@ -11,39 +11,45 @@ export function TeamList(props: TeamListProps) {
 
   return (
     <div className="flex items-center justify-center gap-x-40 pb-64 pl-8 md:flex-nowrap">
-      <div className="flex flex-row gap-16 px-20 py-24 w-[90%] min-w-[300px] max-w-[650px] shadow-lg md:w-[650px] rounded-16">
+      <div className="flex w-[90%] min-w-[300px] max-w-[650px] flex-row gap-16 rounded-16 px-20 py-24 shadow-lg md:w-[650px]">
         {/* アコーディオンボタン */}
         <div>
-          <button className="h-[110px] md:h-64"
+          <button
+            className="h-[110px] md:h-64"
             onClick={() => setIsOpen(!isOpen)}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className={`h-[25px] w-[25px] transition-transform ${isOpen ? 'rotate-90' : ''}`}
+              className={`h-[25px] w-[25px] transition-transform ${isOpen ? "rotate-90" : ""}`}
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5l7 7-7 7"
+              />
             </svg>
           </button>
         </div>
         {/* アコーディオンボタン以外の表示要素 */}
         <div className="flex flex-col">
-          <div className="flex flex-col md:flex-row ml-20 md:ml-40 gap-20 md:gap-40 md:w-[500px] w-[200px] min-w-[150px]">
+          <div className="ml-20 flex w-[200px] min-w-[150px] flex-col gap-20 md:ml-40 md:w-[500px] md:flex-row md:gap-40">
             {/* チーム名表示 */}
-            <div className=" flex flex-[1] flex-col overflow-hidden">
-              <p className="md:pb-20 text-14">チーム名</p>
+            <div className="flex flex-[1] flex-col overflow-hidden">
+              <p className="text-14 md:pb-20">チーム名</p>
               <p
                 className="w-full overflow-hidden truncate whitespace-nowrap text-16 font-bold"
                 title={props.teamName}
               >
                 {props.teamName}
               </p>
-            </div >
+            </div>
             {/* 所属表示 */}
             <div className="flex flex-[1] flex-col overflow-hidden">
-              <p className="md:pb-20 text-14">所属</p>
+              <p className="text-14 md:pb-20">所属</p>
               <p
                 className="text- w-full overflow-hidden truncate whitespace-nowrap font-bold"
                 title={props.affiliation}
@@ -53,11 +59,13 @@ export function TeamList(props: TeamListProps) {
             </div>
           </div>
           {/* 名前表示 */}
-          {isOpen &&(
-            <div className="ml-20 md:ml-40 mt-40">
+          {isOpen && (
+            <div className="ml-20 mt-40 md:ml-40">
               <p className="pb-20">名前</p>
               {props.userNames.map((name, index) => (
-                <p key={index} className="mb-8 font-bold">{name}</p>
+                <p key={index} className="mb-8 font-bold">
+                  {name}
+                </p>
               ))}
             </div>
           )}
