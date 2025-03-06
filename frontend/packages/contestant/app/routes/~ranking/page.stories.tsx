85269f27165b8d4e9bf5fb3cf33801498744d27b
kouxi08 <kouxi14@gmail.com>
2025-03-07 08:51:34 +0900

    add route ranking

--- frontend/packages/contestant/app/routes/~ranking/page.stories.tsx
+++ frontend/packages/contestant/app/routes/~ranking/page.stories.tsx
@@ -1,57 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { RankingItem } from "./page";
+import { RankingPage } from "./page";
 
 export default {
   title: "pages/ranking",
-} satisfies Meta;
+  component: RankingPage,
+} satisfies Meta<typeof RankingPage>;
 
-type Story = StoryObj;
+type Story = StoryObj<typeof RankingPage>;
 
 export const Default: Story = {
-  render: () => (
-    <div className="w-full gap-64">
-      <RankingItem
-        rank={1}
-        teamName="チーム名なまえがわからない"
-        affiliation="testTeam"
-        score={8888}
-        timeStamp={new Date("2025-03-04T12:00:00")}
-      />
-      <RankingItem
-        rank={2}
-        teamName="チーム名がわからないいいいいいいいいいいいい"
-        affiliation="testTeam"
-        score={8888}
-        timeStamp={new Date("2025-03-04T12:00:00")}
-      />
-      <RankingItem
-        rank={3}
-        teamName="チーム名がわからないいいいいいいいいいいいい"
-        affiliation="testTeam"
-        score={8888}
-        timeStamp={new Date("2025-03-04T12:00:00")}
-      />
-      <RankingItem
-        rank={4}
-        teamName="チーム名がわからないいいいいいいいいいいいい"
-        affiliation="testTeam"
-        score={8888}
-        timeStamp={new Date("2025-03-04T12:00:00")}
-      />
-      <RankingItem
-        rank={5}
-        teamName="チーム名がわからないいいいいいいいいいいいい"
-        affiliation="testTeam"
-        score={500}
-        timeStamp={new Date("2025-03-04T12:00:00")}
-      />
-      <RankingItem
-        rank={6}
-        teamName="チーム名がわからないいいいいいいいいいいいい"
-        affiliation="testTeam"
-        score={0}
-        timeStamp={new Date("2025-03-04T12:00:00")}
-      />
-    </div>
-  ),
+    args: {
+      ranking: [
+        ...Array.from({ length: 4 }, () => ({
+          $typeName: "contestant.v1.Rank" as const,
+          rank: BigInt(1),
+          teamName: "チーム名なまえがわからない",
+          score: BigInt(8888),
+          TimeStamp: new Date("2025-03-04T12:00:00"),
+          organization: "testTeam"
+        })),
+      ]
+    }
 };
--- frontend/packages/contestant/app/routes/~ranking/page.tsx
+++ frontend/packages/contestant/app/routes/~ranking/page.tsx
@@ -1,71 +1,73 @@
 import { clsx } from "clsx";
+import type { Rank } from "@ictsc/proto/contestant/v1";
 
-type RankingItemProps = {
-  rank?: number;
-  timeStamp?: Date;
-  teamName: string;
-  affiliation: string;
-  score?: number;
+type RankingProps = {
+  ranking: Rank[];
 };
 
-export function RankingItem(props: RankingItemProps) {
+
+export function RankingPage(props: RankingProps) {
   return (
     <div className="flex w-full flex-wrap items-center justify-center gap-x-40 pb-64 pl-8 md:flex-nowrap">
-      <div>
-        <div className="flex w-[175px] flex-row items-baseline justify-between">
-          {/* ランキング表示 */}
-          <div className="flex flex-row items-baseline">
-            <span className={clsx("text-32 font-bold")}>
-              {props.rank != null ? props.rank : "-"}
-            </span>
-            <p className="text-14 font-bold">st</p>
+      {props.ranking.map((ranking) => (
+        <>
+          <div>
+            <div className="flex w-[175px] flex-row items-baseline justify-between">
+              {/* ランキング表示 */}
+              <div className="flex flex-row items-baseline">
+                <span className={clsx("text-32 font-bold")}>
+                  {ranking.rank}
+                </span>
+                <p className="text-14 font-bold">st</p>
+              </div>
+              {/* トータルスコア表示 */}
+              <div className="flex flex-row items-baseline gap-8">
+                <span
+                  className={clsx(
+                    "text-32 font-bold",
+                    Number(ranking.rank) === 1&& "text-primary",
+                    ranking.score == null && "px-32",
+                  )}
+                >
+                  {ranking.score != null ? ranking.score : "-"}
+                </span>
+                <p className="text-14 font-bold">pt</p>
+              </div>
+            </div>
+            <div className="flex flex-row gap-4">
+              <p className="text-12">Time:</p>
+              <span
+                className={clsx(
+                  "text-12 font-bold",
+                  ranking.timestamp == null && "px-12",
+                )}
+              >
+                {ranking.timestamp != null ? ranking.timestamp.toLocaleString() : "-"}
+              </span>
+            </div>
           </div>
-          {/* トータルスコア表示 */}
-          <div className="flex flex-row items-baseline gap-8">
-            <span
-              className={clsx(
-                "text-32 font-bold",
-                props.rank === 1 && "text-primary",
-                props.score == null && "px-32",
-              )}
-            >
-              {props.score != null ? props.score : "-"}
-            </span>
-            <p className="text-14 font-bold">pt</p>
+          <div className="flex w-[90%] min-w-[300px] max-w-[650px] items-center justify-center gap-16 rounded-16 px-20 py-24 shadow-lg md:w-[650px]">
+            <div className="ml-64 flex w-0 flex-[1] flex-col overflow-hidden">
+              <p className="pb-20 text-14">チーム名</p>
+              <p
+                className="w-full overflow-hidden truncate whitespace-nowrap text-16"
+                title={ranking.teamName}
+              >
+                {ranking.teamName}
+              </p>
+            </div>
+            <div className="flex w-0 flex-[1] flex-col overflow-hidden">
+              <p className="pb-20 text-14">所属</p>
+              <p
+                className="text- w-full overflow-hidden truncate whitespace-nowrap"
+                title={ranking.organization}
+              >
+                {ranking.organization}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-row gap-4">
-          <p className="text-12">Time:</p>
-          <span
-            className={clsx(
-              "text-12 font-bold",
-              props.timeStamp == null && "px-12",
-            )}
-          >
-            {props.timeStamp != null ? props.timeStamp.toLocaleString() : "-"}
-          </span>
-        </div>
-      </div>
-      <div className="flex w-[90%] min-w-[300px] max-w-[650px] items-center justify-center gap-16 rounded-16 px-20 py-24 shadow-lg md:w-[650px]">
-        <div className="ml-64 flex w-0 flex-[1] flex-col overflow-hidden">
-          <p className="pb-20 text-14">チーム名</p>
-          <p
-            className="w-full overflow-hidden truncate whitespace-nowrap text-16"
-            title={props.teamName}
-          >
-            {props.teamName}
-          </p>
-        </div>
-        <div className="flex w-0 flex-[1] flex-col overflow-hidden">
-          <p className="pb-20 text-14">所属</p>
-          <p
-            className="text- w-full overflow-hidden truncate whitespace-nowrap"
-            title={props.affiliation}
-          >
-            {props.affiliation}
-          </p>
-        </div>
-      </div>
+        </>
+      ))}
     </div>
   );
 }
