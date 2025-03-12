d6d6624dea70bb07ee61359035afd7bb0cda8dc9
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-12 14:29:24 +0900

    feat(schedule): add schedule feature

---
+++ frontend/packages/contestant/app/features/schedule/index.ts
@@ -0,0 +1,3 @@
+export { startAt, endAt, fetchSchedule } from "./feature";
+export { ScheduleProvider } from "./provider";
+export { useSchedule } from "./use-schedule";
---
+++ frontend/packages/contestant/app/features/schedule/provider.tsx
@@ -0,0 +1,67 @@
+import { isAfter } from "date-fns";
+import {
+  Suspense,
+  use,
+  useEffect,
+  useState,
+  useTransition,
+  type ReactNode,
+} from "react";
+import { type Schedule } from "@ictsc/proto/contestant/v1";
+import { endAt } from "./feature";
+import { ScheduleContext } from "./use-schedule";
+
+export function ScheduleProvider(props: {
+  readonly initialData: Promise<Schedule | null>;
+  readonly loadData: () => Promise<Schedule | null>;
+  readonly children?: ReactNode;
+}) {
+  const [promiseState, setPromise] = useState({
+    data: props.initialData,
+    base: props.initialData,
+  });
+  const promise = promiseState.base === props.initialData ? promiseState.data : props.initialData;
+  const [isPending, startTransision] = useTransition();
+
+  return (
+    <>
+      <ScheduleContext value={{ promise, isPending }}>
+        {props.children}
+      </ScheduleContext>
+      <Suspense>
+        <ScheduleReloader
+          schedule={promise}
+          load={() => {
+            if (isPending) return;
+            startTransision(() => {
+              setPromise({
+                data: props.loadData(),
+                base: props.initialData,
+              });
+            });
+          }}
+        />
+      </Suspense>
+    </>
+  );
+}
+
+function ScheduleReloader(props: {
+  schedule: Promise<Schedule | null>;
+  load: () => void;
+}) {
+  const { load } = props;
+  const schedule = use(props.schedule);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (schedule == null) return;
+      const end = endAt(schedule);
+      if (end == null || isAfter(new Date(), end)) {
+        load();
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [schedule, load]);
+
+  return null;
+}
