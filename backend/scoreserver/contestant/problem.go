b3d092c04464abd94e9e95e7e312d19389bbd622
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-13 11:46:01 +0900

    feat(deploy): implement contestant.v1.ProblemService/ListDeployments

--- backend/scoreserver/contestant/problem.go
+++ backend/scoreserver/contestant/problem.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ictsc/ictsc-regalia/backend/scoreserver/contestant/session"
 	"github.com/ictsc/ictsc-regalia/backend/scoreserver/domain"
 	"github.com/ictsc/ictsc-regalia/backend/scoreserver/infra/pg"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type ProblemServiceHandler struct {
@@ -17,17 +18,18 @@ type ProblemServiceHandler struct {
 
 	Enforcer *ScheduleEnforcer
 
-	ListEffect ProblemListEffect
-	GetEffect  ProblemGetEffect
+	ListProblemsEffect    ProblemListEffect
+	GetProblemEffect      ProblemGetEffect
+	ListDeploymentsEffect DeploymentsListEffect
 }
 
 var _ contestantv1connect.ProblemServiceHandler = (*ProblemServiceHandler)(nil)
 
 func newProblemServiceHandler(enforcer *ScheduleEnforcer, repo *pg.Repository) *ProblemServiceHandler {
 	return &ProblemServiceHandler{
-		Enforcer:   enforcer,
-		ListEffect: repo,
-		GetEffect:  repo,
+		Enforcer:           enforcer,
+		ListProblemsEffect: repo,
+		GetProblemEffect:   repo,
 	}
 }
 
@@ -51,12 +53,12 @@ func (h *ProblemServiceHandler) ListProblems(
 		return nil, err
 	}
 
-	teamMember, err := domain.UserID(userSess.UserID).TeamMember(ctx, h.ListEffect)
+	teamMember, err := domain.UserID(userSess.UserID).TeamMember(ctx, h.ListProblemsEffect)
 	if err != nil {
 		return nil, err
 	}
 
-	problems, err := teamMember.Team().Problems(ctx, h.ListEffect)
+	problems, err := teamMember.Team().Problems(ctx, h.ListProblemsEffect)
 	if err != nil {
 		return nil, err
 	}
@@ -88,7 +90,7 @@ func (h *ProblemServiceHandler) GetProblem(
 		return nil, err
 	}
 
-	teamMember, err := domain.UserID(userSess.UserID).TeamMember(ctx, h.GetEffect)
+	teamMember, err := domain.UserID(userSess.UserID).TeamMember(ctx, h.GetProblemEffect)
 	if err != nil {
 		return nil, err
 	}
@@ -98,13 +100,13 @@ func (h *ProblemServiceHandler) GetProblem(
 		return nil, err
 	}
 
-	tp, err := teamMember.Team().ProblemDetailByCode(ctx, h.GetEffect, code)
+	teamProblem, err := teamMember.Team().ProblemDetailByCode(ctx, h.GetProblemEffect, code)
 	if err != nil {
 		return nil, err
 	}
-	detail := tp.ProblemDetail()
+	detail := teamProblem.ProblemDetail()
 
-	proto := convertTeamProblem(tp.TeamProblem())
+	proto := convertTeamProblem(teamProblem.TeamProblem())
 	proto.Body = &contestantv1.ProblemBody{
 		Type: contestantv1.ProblemType_PROBLEM_TYPE_DESCRIPTIVE,
 		Body: &contestantv1.ProblemBody_Descriptive{
@@ -116,6 +118,65 @@ func (h *ProblemServiceHandler) GetProblem(
 
 	return connect.NewResponse(&contestantv1.GetProblemResponse{
 		Problem: proto,
+	}), nil
+}
+
+type DeploymentsListEffect interface {
+	domain.TeamMemberGetter
+	domain.ProblemReader
+	domain.DeploymentReader
+}
+
+func (h *ProblemServiceHandler) ListDeployments(
+	ctx context.Context,
+	req *connect.Request[contestantv1.ListDeploymentsRequest],
+) (*connect.Response[contestantv1.ListDeploymentsResponse], error) {
+	userSess, err := session.UserSessionStore.Get(ctx)
+	if err != nil {
+		if errors.Is(err, domain.ErrNotFound) {
+			return nil, connect.NewError(connect.CodeUnauthenticated, nil)
+		}
+		return nil, err
+	}
+
+	protoCode := req.Msg.GetCode()
+	if protoCode == "" {
+		return nil, domain.NewInvalidArgumentError("code is required", nil)
+	}
+
+	teamMember, err := domain.UserID(userSess.UserID).TeamMember(ctx, h.ListDeploymentsEffect)
+	if err != nil {
+		return nil, err
+	}
+
+	code, err := domain.NewProblemCode(protoCode)
+	if err != nil {
+		return nil, err
+	}
+
+	teamProblem, err := teamMember.Team().ProblemDetailByCode(ctx, h.ListDeploymentsEffect, code)
+	if err != nil {
+		return nil, err
+	}
+
+	deployments, err := teamProblem.TeamProblem().Deployments(ctx, h.ListDeploymentsEffect)
+	if err != nil {
+		return nil, err
+	}
+
+	protoDeployments := make([]*contestantv1.DeploymentRequest, 0, len(deployments))
+	for _, deployment := range deployments {
+		protoDeployments = append(protoDeployments, &contestantv1.DeploymentRequest{
+			Revision:            deployment.Revision(),
+			Status:              convertDeploymentStatus(deployment.Status()),
+			RequestedAt:         timestamppb.New(deployment.CreatedAt()),
+			AllowedRequestCount: teamProblem.TeamProblem().RemainingDeployments(deployment.Revision()),
+			Penalty:             teamProblem.TeamProblem().Penalty(deployment.Revision()),
+		})
+	}
+
+	return connect.NewResponse(&contestantv1.ListDeploymentsResponse{
+		Deployments: protoDeployments,
 	}), nil
 }
 
@@ -141,4 +202,21 @@ func convertTeamProblem(problem *domain.TeamProblem) *contestantv1.Problem {
 		proto.Deployment.PenaltyThreashold = problem.PercentagePenalty().Threshold
 	}
 	return proto
+}
+
+func convertDeploymentStatus(status domain.DeploymentStatus) contestantv1.DeploymentStatus {
+	switch status {
+	case domain.DeploymentStatusQueued:
+		fallthrough
+	case domain.DeploymentStatusCreating:
+		return contestantv1.DeploymentStatus_DEPLOYMENT_STATUS_DEPLOYING
+	case domain.DeploymentStatusCompleted:
+		return contestantv1.DeploymentStatus_DEPLOYMENT_STATUS_DEPLOYED
+	case domain.DeploymentStatusFailed:
+		return contestantv1.DeploymentStatus_DEPLOYMENT_STATUS_FAILED
+	case domain.DeploymentStatusUnknown:
+		fallthrough
+	default:
+		return contestantv1.DeploymentStatus_DEPLOYMENT_STATUS_UNSPECIFIED
+	}
 }
