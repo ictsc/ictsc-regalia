488a50c450528a5c889665c7e4fa8a5e7e1d2996
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-15 05:01:46 +0900

    feat: add ranking view

--- frontend/packages/contestant/app/routes/~ranking/page.tsx
+++ frontend/packages/contestant/app/routes/~ranking/page.tsx
@@ -18,25 +18,38 @@ const formatter = new Intl.DateTimeFormat("ja-JP", {
   timeStyle: "medium",
 });
 
+const rankPruralRule = new Intl.PluralRules("en-US", { type: "ordinal" });
+
 export function RankingPage(props: RankingProps) {
   return (
     <>
       <Title>ランキング</Title>
-      <div className="flex h-[800px] flex-col items-center pl-8 md:flex-nowrap">
+      <div className="my-40 flex size-full flex-col items-center px-20 md:flex-nowrap">
         {props.ranking.map((ranking, index) => {
           const { rank, score } = ranking;
 
           return (
             <div
               key={index}
-              className="flex flex-wrap items-center gap-x-40 pb-64"
+              className="flex w-full flex-col gap-x-40 pb-64 lg:flex-row lg:items-center"
             >
-              <div className="flex flex-col">
-                <div className="flex w-[175px] gap-x-40">
+              <div className="flex w-[175px] flex-col">
+                <div className="flex justify-between gap-x-40">
                   {/* ランキング表示 */}
                   <div className="flex flex-row items-baseline">
                     <span className={clsx("text-32 font-bold")}>{rank}</span>
-                    <p className="text-14 font-bold">st</p>
+                    <p className="text-14 font-bold">
+                      {
+                        {
+                          zero: "",
+                          one: "st",
+                          two: "nd",
+                          few: "rd",
+                          many: "th",
+                          other: "th",
+                        }[rankPruralRule.select(rank)]
+                      }
+                    </p>
                   </div>
                   {/* トータルスコア表示 */}
                   <div className="flex flex-row items-baseline gap-8">
@@ -52,7 +65,7 @@ export function RankingPage(props: RankingProps) {
                     <p className="text-14 font-bold">pt</p>
                   </div>
                 </div>
-                <div className="flex flex-row gap-4">
+                <div className="flex flex-row justify-between gap-4">
                   <p className="text-12">Time:</p>
                   <span
                     className={clsx(
@@ -68,20 +81,20 @@ export function RankingPage(props: RankingProps) {
                   </span>
                 </div>
               </div>
-              <div className="flex w-[90%] min-w-[300px] max-w-[650px] items-center justify-center gap-16 rounded-16 px-20 py-24 shadow-lg md:w-[650px]">
-                <div className="ml-64 flex w-0 flex-[1] flex-col overflow-hidden">
-                  <p className="pb-20 text-14">チーム名</p>
+              <div className="flex flex-col sm:flex-row w-full min-w-[300px] max-w-[650px] sm:items-center justify-center gap-16 rounded-16 px-20 py-24 shadow-lg">
+                <div className="flex flex-1 flex-col justify-self-center overflow-hidden">
+                  <p className="text-14">チーム名</p>
                   <p
-                    className="w-full overflow-hidden truncate whitespace-nowrap text-16"
+                    className="mt-20 w-full overflow-hidden truncate whitespace-nowrap text-16"
                     title={ranking.teamName}
                   >
                     {ranking.teamName}
                   </p>
                 </div>
-                <div className="flex w-0 flex-[1] flex-col overflow-hidden">
-                  <p className="pb-20 text-14">所属</p>
+                <div className="flex flex-1 flex-col overflow-hidden">
+                  <p className="text-14">所属</p>
                   <p
-                    className="text- w-full overflow-hidden truncate whitespace-nowrap"
+                    className="mt-20 w-full overflow-hidden truncate whitespace-nowrap text-16"
                     title={ranking.organization}
                   >
                     {ranking.organization}
