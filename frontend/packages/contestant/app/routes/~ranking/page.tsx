060cc90e266b79e3789bd8eae2eecfb0bbb02912
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-15 05:28:26 +0900

    feat: grid ranking page

--- frontend/packages/contestant/app/routes/~ranking/page.tsx
+++ frontend/packages/contestant/app/routes/~ranking/page.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { clsx } from "clsx";
 import { Title } from "../../components/title";
 
@@ -24,86 +25,89 @@ export function RankingPage(props: RankingProps) {
   return (
     <>
       <Title>ランキング</Title>
-      <div className="my-40 flex size-full flex-col items-center px-20 md:flex-nowrap">
-        {props.ranking.map((ranking, index) => {
-          const { rank, score } = ranking;
+      <div className="my-40 flex size-full flex-col items-center px-20">
+        <div className="grid w-full max-w-screen-md grid-cols-1 gap-40 md:grid-cols-[auto_1fr]">
+          {props.ranking.map((ranking, index) => {
+            const { rank, score } = ranking;
 
-          return (
-            <div
-              key={index}
-              className="flex w-full flex-col gap-x-40 pb-64 lg:flex-row lg:items-center"
-            >
-              <div className="flex w-[175px] flex-col">
-                <div className="flex justify-between gap-x-40">
-                  {/* ランキング表示 */}
-                  <div className="flex flex-row items-baseline">
-                    <span className={clsx("text-32 font-bold")}>{rank}</span>
-                    <p className="text-14 font-bold">
-                      {
+            return (
+              <Fragment key={index}>
+                <div className="flex flex-col self-center">
+                  <div className="flex gap-x-40 md:justify-between">
+                    {/* ランキング表示 */}
+                    <div className="flex flex-row items-baseline">
+                      <span className={clsx("text-32 font-bold")}>{rank}</span>
+                      <p className="text-14 font-bold">
                         {
-                          zero: "",
-                          one: "st",
-                          two: "nd",
-                          few: "rd",
-                          many: "th",
-                          other: "th",
-                        }[rankPruralRule.select(rank)]
-                      }
-                    </p>
+                          {
+                            zero: "",
+                            one: "st",
+                            two: "nd",
+                            few: "rd",
+                            many: "th",
+                            other: "th",
+                          }[rankPruralRule.select(rank)]
+                        }
+                      </p>
+                    </div>
+                    {/* トータルスコア表示 */}
+                    <div className="flex flex-row items-baseline gap-8">
+                      <span
+                        className={clsx(
+                          "text-32 font-bold",
+                          rank === 1 && "text-primary",
+                          score == null && "px-32",
+                        )}
+                      >
+                        {score != null ? score : "-"}
+                      </span>
+                      <p className="text-14 font-bold">pt</p>
+                    </div>
                   </div>
-                  {/* トータルスコア表示 */}
-                  <div className="flex flex-row items-baseline gap-8">
+                  <div className="flex flex-row gap-4 md:justify-between">
+                    <p className="text-12">Time:</p>
                     <span
                       className={clsx(
-                        "text-32 font-bold",
-                        rank === 1 && "text-primary",
-                        score == null && "px-32",
+                        "text-12 font-bold",
+                        ranking.lastEffectiveSubmitAt == null && "px-12",
                       )}
                     >
-                      {score != null ? score : "-"}
+                      {ranking.lastEffectiveSubmitAt
+                        ? formatter.format(
+                            new Date(ranking.lastEffectiveSubmitAt),
+                          )
+                        : "-"}
                     </span>
-                    <p className="text-14 font-bold">pt</p>
                   </div>
                 </div>
-                <div className="flex flex-row justify-between gap-4">
-                  <p className="text-12">Time:</p>
-                  <span
-                    className={clsx(
-                      "text-12 font-bold",
-                      ranking.lastEffectiveSubmitAt == null && "px-12",
-                    )}
-                  >
-                    {ranking.lastEffectiveSubmitAt
-                      ? formatter.format(
-                          new Date(ranking.lastEffectiveSubmitAt),
-                        )
-                      : "-"}
-                  </span>
+                <div className="flex flex-col justify-center gap-16 rounded-16 p-24 shadow-lg sm:flex-row sm:items-center">
+                  <div className="flex flex-1 sm:justify-center">
+                    <div className="flex flex-col justify-self-center overflow-hidden">
+                      <p className="text-14">チーム名</p>
+                      <p
+                        className="mt-20 w-full overflow-hidden truncate whitespace-nowrap text-16"
+                        title={ranking.teamName}
+                      >
+                        {ranking.teamName}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-1 sm:justify-center">
+                    <div className="flex flex-col overflow-hidden">
+                      <p className="text-14">所属</p>
+                      <p
+                        className="mt-20 w-full overflow-hidden truncate whitespace-nowrap text-16"
+                        title={ranking.organization}
+                      >
+                        {ranking.organization}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="flex w-full min-w-[300px] max-w-[650px] flex-col justify-center gap-16 rounded-16 px-20 py-24 shadow-lg sm:flex-row sm:items-center">
-                <div className="flex flex-1 flex-col justify-self-center overflow-hidden">
-                  <p className="text-14">チーム名</p>
-                  <p
-                    className="mt-20 w-full overflow-hidden truncate whitespace-nowrap text-16"
-                    title={ranking.teamName}
-                  >
-                    {ranking.teamName}
-                  </p>
-                </div>
-                <div className="flex flex-1 flex-col overflow-hidden">
-                  <p className="text-14">所属</p>
-                  <p
-                    className="mt-20 w-full overflow-hidden truncate whitespace-nowrap text-16"
-                    title={ranking.organization}
-                  >
-                    {ranking.organization}
-                  </p>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+              </Fragment>
+            );
+          })}
+        </div>
       </div>
     </>
   );
