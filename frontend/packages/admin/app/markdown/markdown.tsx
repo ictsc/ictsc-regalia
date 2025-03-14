f0391341890ec32cad672ec2515087be03bf31c3
Fuuma <masa81hiro69@gmail.com>
2025-03-14 21:11:59 +0900

    fix: remove unnecessary blank line in Markdown component

--- frontend/packages/admin/app/markdown/markdown.tsx
+++ frontend/packages/admin/app/markdown/markdown.tsx
@@ -1,7 +1,6 @@
 import { use, useMemo } from "react";
 import { renderMarkdown } from "./markdown-utils";
 
-
 export function Markdown({ children }: { children?: string }) {
   const nodePromise = useMemo(() => renderMarkdown(children ?? ""), [children]);
   return use(nodePromise);
