626755726f721348228a768b50f2aba45002e08f
tosuke <13393900+tosuke@users.noreply.github.com>
2025-03-14 17:48:22 +0900

    feat(score): update answer scores

--- backend/scoreserver/domain/score.go
+++ backend/scoreserver/domain/score.go
@@ -1,5 +1,13 @@
 package domain
 
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/gofrs/uuid/v5"
+)
+
 type Score struct {
 	max     uint32
 	marked  uint32
@@ -15,17 +23,76 @@ func (s *Score) MaxScore() uint32 {
 }
 
 func (s *Score) Penalty() uint32 {
-	return s.penalty // 現状は再展開がないので0
+	return s.penalty
 }
 
 func (s *Score) TotalScore() uint32 {
 	return max(0, s.MarkedScore()-s.Penalty())
 }
 
-type ScoreData struct {
-	MarkedScore uint32 `json:"marked_score"`
-	Penalty     uint32 `json:"penalty"`
+type UpdateAnswerScoreEffect interface {
+	MarkingResultReader
+	ScoreWriter
 }
+
+func (a *Answer) UpdateScore(ctx context.Context, eff UpdateAnswerScoreEffect, now time.Time) error {
+	var errs []error
+	if err := a.updatePrivateScore(ctx, eff); err != nil {
+		errs = append(errs, err)
+	}
+	if err := a.updatePublicScore(ctx, eff, now); err != nil {
+		errs = append(errs, err)
+	}
+	return errors.Join(errs...)
+}
+
+func (a *Answer) updatePublicScore(ctx context.Context, eff UpdateAnswerScoreEffect, now time.Time) error {
+	latestPublicMark, err := a.latestPublicMarkingResult(ctx, eff, now)
+	if err != nil {
+		if errors.Is(err, ErrNotFound) {
+			return nil
+		}
+	}
+	if err := eff.UpdatePublicAnswerScore(ctx, &UpdateAnswerScoreInput{
+		AnswerID:        a.id,
+		MarkingResultID: latestPublicMark.id,
+	}); err != nil {
+		return WrapAsInternal(err, "failed to update answer score")
+	}
+	return nil
+}
+
+func (a *Answer) updatePrivateScore(ctx context.Context, eff UpdateAnswerScoreEffect) error {
+	latestMark, err := a.latestMarkingResult(ctx, eff)
+	if err != nil {
+		if errors.Is(err, ErrNotFound) {
+			return nil
+		}
+	}
+	if err := eff.UpdatePrivateAnswerScore(ctx, &UpdateAnswerScoreInput{
+		AnswerID:        a.id,
+		MarkingResultID: latestMark.id,
+	}); err != nil {
+		return WrapAsInternal(err, "failed to update answer score")
+	}
+	return nil
+}
+
+type (
+	ScoreData struct {
+		MarkedScore uint32 `json:"marked_score"`
+		Penalty     uint32 `json:"penalty"`
+	}
+
+	UpdateAnswerScoreInput struct {
+		AnswerID        uuid.UUID
+		MarkingResultID uuid.UUID
+	}
+	ScoreWriter interface {
+		UpdatePrivateAnswerScore(ctx context.Context, input *UpdateAnswerScoreInput) error
+		UpdatePublicAnswerScore(ctx context.Context, input *UpdateAnswerScoreInput) error
+	}
+)
 
 func (s *Score) Data() *ScoreData {
 	return &ScoreData{
@@ -39,7 +106,8 @@ func (s *ScoreData) parse(problem *Problem) (*Score, error) {
 		return nil, NewInvalidArgumentError("marked score is over max score", nil)
 	}
 	return &Score{
-		max:    problem.maxScore,
-		marked: s.MarkedScore,
+		max:     problem.maxScore,
+		marked:  s.MarkedScore,
+		penalty: s.Penalty,
 	}, nil
 }
